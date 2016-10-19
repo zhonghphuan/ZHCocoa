4ac19a7931ac9c157fc1e16d0b53f32c570978d9
zhonghuan <zhonghphuan@hotmail.com>
2016-10-19 03:24:18 -0700

    调整

---
+++ ZHCocoa/ZHCocoa/Main/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZHCocoa
+//
+//  Created by HM on 16/10/19.
+//  Copyright © 2016年 HM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
